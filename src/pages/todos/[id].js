868b65971b85dfbe2c8f62752e0f6dadbadb51d9
Iyanu Henry famoritiye <henry.famoritiye@gmail.com>
2024-08-07 15:23:03 +0100

    Final build and fix for deployment

--- src/pages/todos/[id].js
+++ src/pages/todos/[id].js
@@ -30,10 +30,10 @@ const TodoDetail = () => {
 
   return (
     <div className="w-screen h-screen flex justify-center items-center">
-      <div className="container mx-auto p-4 flex flex-col   border-4 w-full sm:w-2/3 lg:w-1/2    ">
+      <div className="container mx-auto p-4 flex flex-col border-4 w-full sm:w-2/3 lg:w-1/2">
         <h1 className="text-2xl font-bold mb-4 text-center">Todo Detail</h1>
         <div className="p-4 border-4 rounded">
-          <h2 className="text-xl ">{todo.title}</h2>
+          <h2 className="text-xl">{todo.title}</h2>
           <p>ID: {todo.id}</p>
           <p>Status: {todo.completed ? "Completed" : "Incomplete"}</p>
         </div>
