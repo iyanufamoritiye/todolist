9b74bc53e20fe0da99d5163492d0ce87fcdc6b4f
Iyanu Henry famoritiye <henry.famoritiye@gmail.com>
2024-08-09 18:02:58 +0100

    first commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -73,6 +73,7 @@ const Home = () => {
         Todo List
       </h1>
       <TodoForm onSubmit={addTodo} />
+      {error && <p className="text-red-500 text-center">{error}</p>}
       <div className="mt-4 space-y-4">
         {todos.map((todo) => (
           <TodoItem
